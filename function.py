fd4ba5b2577176aaf9dbb5d9b93c91676b315ba5
Piotr Niewinski <me@piotrniewinski.com>
2021-10-19 14:06:31 +0200

    tested searching with hpmser OMP

--- function.py
+++ function.py
@@ -71,16 +71,16 @@ def plot_func(
 if __name__ == '__main__':
 
     # base 2dim examples
-    plot_func(ndim=2, rng=0.5,  sscl=0,     n_samples=100)
-    plot_func(ndim=2, rng=0.5,  sscl=50,    n_samples=100)
-    plot_func(ndim=2, rng=0.5,  sscl=30,    n_samples=500)
-    plot_func(ndim=2, rng=2.0,  sscl=30,    n_samples=100)
-    plot_func(ndim=2, rng=2.0,  sscl=30,    n_samples=1000)
-    plot_func(ndim=2, rng=2.0,  sscl=0,     n_samples=1000)
+    #plot_func(ndim=2, rng=0.5,  sscl=0,     n_samples=100)
+    #plot_func(ndim=2, rng=0.5,  sscl=50,    n_samples=100)
+    #plot_func(ndim=2, rng=0.5,  sscl=30,    n_samples=500)
+    #plot_func(ndim=2, rng=2.0,  sscl=30,    n_samples=100)
+    #plot_func(ndim=2, rng=2.0,  sscl=30,    n_samples=1000)
+    #plot_func(ndim=2, rng=2.0,  sscl=0,     n_samples=1000)
 
     # more dims
     #plot_func(ndim=2, rng=0.7, sscl=0,      n_samples=3000)
     #plot_func(ndim=3, rng=0.7, sscl=0,      n_samples=3000)
     #plot_func(ndim=5, rng=0.7, sscl=0,      n_samples=3000)
 
-    #plot_func(ndim=2, rng=5.0, sscl=0,      n_samples=3000)
+    plot_func(ndim=2, rng=5.0, sscl=0,      n_samples=3000)
--- searching.py
+++ searching.py
@@ -1,4 +1,4 @@
-from ptools.pms.hpmser.search_function import hpmser_GX
+from ptools.pms.hpmser.search_function import hpmser_GX, hpmser_GX_OMP
 
 from psdd import get_psdd
 from function import rastrigin_func_ndim
@@ -9,7 +9,17 @@ if __name__ == '__main__':
 
     psdd = get_psdd(n_dim=2, rng=0.7)
 
+    """
     hpmser_GX(
+        func=           rastrigin_func_ndim,
+        func_defaults=  {'sleep':None},
+        psdd=           psdd,
+        devices=        [None]*20,
+        #preferred_axes= ['p1','p2'],
+        verb=           1)
+    """
+
+    hpmser_GX_OMP(
         func=           rastrigin_func_ndim,
         func_defaults=  {'sleep':None},
         psdd=           psdd,
