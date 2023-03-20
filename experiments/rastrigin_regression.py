02ee84ca649c52b8d1818ebfcddcc5050737b5c6
Piotr Niewinski <p.niewinski@edrone.me>
2023-03-20 21:59:20 +0100

    upd

--- experiments/rastrigin_regression.py
+++ experiments/rastrigin_regression.py
@@ -5,7 +5,7 @@ from sklearn.svm import SVR
 from function import rastrigin_func_ndim
 
 
-n_samples = 500
+n_samples = 20
 range = 5
 
 d = range*np.random.rand(n_samples,2)-range/2
@@ -23,13 +23,13 @@ three_dim(tri)
 
 svr_rbf = SVR(
     kernel=     "rbf",
-    C=          1,      # 10^-3 - 10^3, regularization, controls error with margin, lower C -> larger margin
-    gamma=      5,      # 10^-3 - 10^3, controls shape of decision boundary, larger for more complex
+    C=          0.01,   # 10^-3 - 10^3, regularization, controls error with margin, lower C -> larger margin
+    gamma=      1,      # 10^-3 - 10^3, controls shape of decision boundary, larger for more complex
     epsilon=    0.01,   # threshold for what is considered an acceptable error rate in the training data
 )
 svr_rbf.fit(d, val)
 
-d = range*np.random.rand(10*n_samples,2)-range/2
+d = range*np.random.rand(2000,2)-range/2
 val = svr_rbf.predict(d)
 
 tri = np.asarray([(x,y,v) for (x,y),v in zip(d,val)])
